12add8e3722fe47043aec109a16d889f55b85c3c
Lavinia Trabuco <laviniapedrosat@gmail.com>
2025-11-15 21:51:37 -0300

    [api/infrastructure] config mongo db and docker

---
+++ src/CaseFinx.Infrastructure/Mongo/MongoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CaseFinx.Domain.Entities;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+
+namespace CaseFinx.Infrastructure.Mongo
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration config)
+        {
+            var connectionString = config["Mongo:ConnectionString"];
+            var databaseName = config["Mongo:Database"];
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Paciente> Pacientes =>
+            _database.GetCollection<Paciente>("pacientes");
+
+        public IMongoCollection<HistoricoPaciente> Historicos =>
+            _database.GetCollection<HistoricoPaciente>("historicos");
+    }
+}
